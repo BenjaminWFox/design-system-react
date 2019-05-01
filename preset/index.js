cf3aff84dc9133f933cdd096091ea4e5bb6e5c18
Ben Fox <benjamin.fox@smashingideas.com>
2019-05-01 07:04:58 -0700

    Update preset

--- preset/index.js
+++ preset/index.js
@@ -7,18 +7,20 @@
 module.exports = function buildPreset() {
 	return {
 		presets: [
-			require('babel-preset-env').default(null, {
+			require('@babel/preset-env').default(null, {
 				targets: {
 					browsers: ['last 2 versions', 'ie 11'],
 					node: '8.9.4',
 				},
 				modules: false,
 			}),
-			require('babel-preset-react'),
+			require('@babel/preset-react'),
 		],
 		plugins: [
-			require('babel-plugin-transform-object-rest-spread'),
-			require('babel-plugin-transform-class-properties'),
+			require('@babel/plugin-proposal-object-rest-spread'),
+			require('@babel/plugin-proposal-class-properties'),
+			require('@babel/plugin-proposal-export-default-from'),
+			require('@babel/plugin-proposal-export-namespace-from'),
 			require('babel-plugin-transform-export-extensions'),
 		],
 	};
